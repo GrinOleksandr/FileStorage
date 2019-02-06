423df9764dc68ccbd4086d67d1ceaa9e9fbfb7fa
GrinOleksandr <sasha.lisove@gmail.com>
2019-02-06 15:16:30 +0200

    res

--- server/app.js
+++ server/app.js
@@ -1,16 +1,49 @@
 'use strict';
 const FilesStorageDb = require('./db'),
     config = require('./config'),
-    app = require('express')(),
-    bodyParser = require('body-parser');
+    express = require('express'),
+    app =express(),
+    bodyParser = require('body-parser'),
+    fileUpload = require('express-fileupload');
+
+
 
 // server Setup
 const http = require('http'),
-    url = require('url');
-    // port = 8000,
-    // ip = '127.0.0.1';
+    url = require('url'),
+    fs = require('fs'),
+    path = require('path');
 
-app.post ('/add', function(request,response) {
+
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+app.use('/upload', function(req, res) {
+    if (Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    console.log(req.files);
+    let sampleFile = req.files.sampleFile;
+    console.log(req.files[0]);
+    console.log(req.files[1]);
+    console.log(req.files[2]);
+    console.log(req.files[3]);
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(`${__dirname}/filestorage/${req.files.sampleFile.name}`, function(err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+});
+
+
+
+
+
+app.use ('/add', function(request,response) {
     // response.send('HelloWorld');
     response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': "*"});
     let parsedUrl = url.parse(request.url, true);
@@ -26,7 +59,74 @@ app.post ('/add', function(request,response) {
 
 });
 
+app.use ('/getfiles', function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': "*"});
+    FilesStorageDb.find({})
+             .select('-_id -__v')
+             .exec(function(err,Result){
 
+            // console.log(Result[0].toString());
+            let responseString =  JSON.stringify(Result);
+            console.log(responseString);
+                 response.end(responseString);
+
+        });
+});
+
+////////////////////////////////////////////////////////EXPERIMENT!
+app.use ('/test', function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': "*"});
+
+    let responseString = request.files;
+    console.log(responseString);
+    response.end(responseString);
+
+});
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////EXPERIMENT!
+
+
+
+//Files handlers
+//
+// app.use('/readfile', 'utf8', function(request,response){
+//     let parsedUrl = url.parse(request.url, true);
+//     let file = `../../server/${parsedUrl.query.name}`;
+//     fs.readFile(file, function(err, data){
+//         if(err) throw err;
+//         console.log(path.resolve(__dirname, 'server'));
+//         response.send(data);
+//     })
+//
+// });
+
+
+// app.use('/upload', function(request, response){
+//     response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': "*"});
+//
+//
+//     let uploadedFile = request.files.uploadedFile;
+//     console.log(uploadedFile);
+//     uploadedFile.mv(`/123.txt`, function(err){
+//         if (err)
+//             return response.status(500).send(err);
+//
+//         response.send('File uploaded!');
+//     });
+//     // let responseString = request.files;
+//     // console.log(responseString);
+//     // response.end(responseString);
+//     // console.log("File UPLOADED!!")
+// });
 
 
 
@@ -70,7 +170,7 @@ app.post ('/add', function(request,response) {
 //         });
 //     }
 // });
-// server.listen(config.port,config.ip);
+app.listen(config.port,config.ip);
 
 console.log(`*****Server running at ${config.ip}:${config.port}`);
 
--- src/index.js
+++ src/index.js
@@ -11,6 +11,151 @@ const ListFiles = document.getElementById('get-files');
 ListFiles.addEventListener('click', listFiles );
 
 const ListOfFiles = document.getElementById('files-list');
+// DRAG&DROP
+const DropZone = document.getElementById("upload-container");
+// DropZone.on('drag dragstart dragend dragover dragenter dragleave drop', function(){
+//     return false;
+// });
+DropZone.addEventListener('dragover', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    DropZone.classList.add('dragover');
+});
+DropZone.addEventListener('dragenter', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    DropZone.classList.add('dragover');
+});
+
+DropZone.addEventListener('dragleave', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    let dx = e.pageX - DropZone.getBoundingClientRect().left + pageXOffset;
+    let dy = e.pageY - DropZone.getBoundingClientRect().top + pageYOffset;
+    if ((dx < 0) || (dx > DropZone.clientWidth) || (dy < 0) || (dy > DropZone.clientHeight)) {
+        DropZone.classList.remove('dragover');
+    }
+});
+const FileInput = document.getElementById("file-input");
+FileInput.addEventListener('focus', ()=> DropZone.querySelector('label').classList.add('focus'));
+FileInput.addEventListener('blur', ()=> DropZone.querySelector('label').classList.remove('focus'));
+//upload via input field
+FileInput.addEventListener('change', function(){
+    // let files = this.files;
+    // sendFiles(files);
+    DropZone.submit();
+});
+
+//upload via drag&drop
+DropZone.addEventListener('drop', function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    sendFiles(e.dataTransfer.files)
+    // FileInput.value = e.dataTransfer.files;
+});
+///////////////////////////////////////////////////////////////////////////////TESTTTTT
+const form = document.getElementById('test');
+const input = document.getElementById('filefile');
+const inputBtn = document.getElementById('filefilebtn');
+inputBtn.addEventListener('click', add);
+
+function add(){
+    console.log(input.files);
+    // fetch("http://127.0.0.1:8000/test", {
+    //         method: 'POST',
+    //         body:input.files[0]
+    //     }).then(function (response) {
+    //         return response.text()
+    //     }).then(function(response){
+    //         console.log(response)
+    //     })
+    form.submit();
+
+}
+//////new SENDFILES
+function sendFiles(file){
+    console.log(file);
+
+    DropZone.classList.remove('dragover');
+    let temp = new ClipboardEvent('').clipboardData || // Firefox < 62 workaround exploiting https://bugzilla.mozilla.org/show_bug.cgi?id=1422655
+        new DataTransfer(); // specs compliant (as of March 2018 only Chrome)
+    // [].forEach.call(file, function(item, i, arr){
+    temp.items.add(new File([file], file.name));
+    // });
+    console.log(temp.files);
+    FileInput.files = temp.files;
+
+    fetch("http://127.0.0.1:8000/test", {
+        method: 'POST',
+        body:file
+    }).then(function (response) {
+        return response.text()
+    }).then(function(response){
+        console.log(response)
+    })
+
+//////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////TESTTTTT
+
+
+
+
+
+// function sendFiles(file){
+//     console.log(file);
+//
+//     DropZone.classList.remove('dragover');
+//     let temp = new ClipboardEvent('').clipboardData || // Firefox < 62 workaround exploiting https://bugzilla.mozilla.org/show_bug.cgi?id=1422655
+//         new DataTransfer(); // specs compliant (as of March 2018 only Chrome)
+//     // [].forEach.call(file, function(item, i, arr){
+//         temp.items.add(new File([file], file.name));
+//     // });
+//     console.log(temp.files);
+//     FileInput.files = temp.files;
+//     DropZone.submit();
+
+
+
+// DropZone.submit();
+
+    // let data = new FormData();
+    // [].forEach.call(files, function(item, i, arr){
+    //     data.append('images',files);
+    // });
+
+    // data.append('name', file.name);
+    // data.append('type', file.type);
+    // data.append('size', file.size);
+    // data.append('file', file);
+    // console.log(data);
+
+    // const xhr = new XMLHttpRequest();
+    // xhr.open('POST',"http://127.0.0.1:8000/upload", true);
+    // xhr.send(data);
+    // fetch("http://127.0.0.1:8000/upload?", {
+    //     method: 'POST'
+    // }).then(function (response) {
+    //     return response.text()
+    // }).then(function(response){
+    //     console.log(response)
+    // })
+    // let form=new FormData();
+    // let xhr = new XMLHttpRequest();
+    // form.append("file",files);
+    // xhr.open("post","/upload",true);
+    // xhr.send(form);
+
+}
+
+
 
 function postFile(name,type){
     let urlString = `http://127.0.0.1:8000/add?name=${name}&type=${type}`;
@@ -20,9 +165,6 @@ function postFile(name,type){
         .catch(error => console.log("Данные не отправленны: " + error));
 
     console.log(`${name}.${type}   OK`)
-
-
-
 }
 
 function listFiles(){
@@ -34,7 +176,7 @@ function listFiles(){
 
 
 function renderList () {
-    fetch("http://127.0.0.1:8000/get?", {
+    fetch("http://127.0.0.1:8000/getfiles?", {
         method: 'GET'
     }).then(function (response) {
         return response.text()
