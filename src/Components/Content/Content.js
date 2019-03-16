939e74ed817f7de06b0a13e7cf211507a3bc15f1
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-16 14:34:34 +0200

    fileUploads not working

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -7,40 +7,20 @@ class Content extends Component {
     constructor(props){
         super(props);
 
-        this.downloadFile = this.downloadFile.bind(this);
-        this.fileClick = this.fileClick.bind(this);
-
-        // this.uploadString = `${window.location.origin}/upload`;
+        // this.downloadFile = this.downloadFile.bind(this);
+        // this.fileClick = this.fileClick.bind(this);
         this.state = { MyFiles: "" };
-    }
-
-    componentDidMount() {
-        fetch("http://127.0.0.1:8000/file/getfiles?", {
-            method: 'GET'
-        }).then(function (response) {
-            return response.text()
-        })
-            .then(function (textOfResponse) {
-                return JSON.parse(textOfResponse);
-            })
-            .then((array) => this.setState({MyFiles : array })
-            )
-            .catch(error => console.log("Данные не получены: " + error));
     }
 
     render() {
         return (
 
             <div id='container1'>
-    <div id='sidebar'>Sidebar</div>
-    <div id='content'>
-        <UploadFiles />
+                <UploadFiles/>
 
-        <button id="readfile">ReadFile</button>
-        <button id="get-files">GET</button>
 
-    </div>
-</div>
+            </div>
+
 
         );
     }
