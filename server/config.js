612380e9ef36cafc2be02fed931feaf7a918bf1d
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-06 08:34:40 +0300

    aaaa

--- server/config.js
+++ server/config.js
@@ -7,7 +7,6 @@ module.exports = {
     ip:"localhost",
     port: process.env.PORT || 8000,
     mongoUrl: `mongodb+srv://filex:Filex123@testclusterscv-rokh7.mongodb.net/test`,
-    fileStoragePath: `./server/filestorage/`
+    fileStoragePath: `${__dirname}/filestorage/`
 };
 
-// fileStoragePath: `${__dirname}/filestorage/`
